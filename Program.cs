a49ca7e0fc54af2ba9c2393fcd05584feb620328
Els <Emathivannan@spartaglobal.com>
2018-12-07 18:48:10 +0000

    Setup advance calculator

--- Program.cs
+++ Program.cs
@@ -18,6 +18,13 @@ namespace Calculator
           {
              break;
           }
+          //initialise the firstNumber and secondNumber variable has a 'double' so for calculation
+          string firstEntry;
+          string secondEntry;
+          double firstNumber;
+          double secondNumber;
+          string operation;
+          double answer;
           Console.WriteLine(name + " type 'b/a' if you would like to perform a (b)asic or (a)dvanced calculation");
           var which = Console.ReadLine();
           // Basic Calculator
@@ -26,15 +33,11 @@ namespace Calculator
             //Prompt the user to type in their first number
             Console.Write(name + ", can you please type your first number ");
             //save it to this variable
-            var firstEntry = Console.ReadLine();
-            //initialise the firstNumber variable has a 'double' so it can calculate any number
-            double firstNumber;
+            firstEntry = Console.ReadLine();
             // Prompt the user to type in their second number
             Console.Write(name + ", can you please type your second number ");
             //save it to this variable
-            var secondEntry = Console.ReadLine();
-            //initialise the secondNumber var as a 'double'
-            double secondNumber;
+            secondEntry = Console.ReadLine();
             // check if the user typed in an input which meets the requirements of the program, if not the program tells the user incorrect input was provided and restarts the program
             try
             {
@@ -52,32 +55,32 @@ namespace Calculator
             }
             // Prompt the user to select an operator
             Console.Write(name + "can you please select an operator +, -, * or /: ");
-            var operation = Console.ReadLine();
+            operation = Console.ReadLine();
             // Addition
             if (operation == "+")
             {
-              var answer = firstNumber + secondNumber;
+              answer = firstNumber + secondNumber;
               Console.WriteLine(name + " your answer is " + answer);
               break;
             }
             // Subtraction
             else if (operation == "-")
             {
-              var answer = firstNumber - secondNumber;
+              answer = firstNumber - secondNumber;
               Console.WriteLine(name + " your answer is " + answer);
               break;
             }
             // Multiplication
             else if (operation == "*")
             {
-              var answer = firstNumber * secondNumber;
+              answer = firstNumber * secondNumber;
               Console.WriteLine(name + " your answer is " + answer);
               break;
             }
             // Division
             else if (operation == "/")
             {
-              var answer = firstNumber / secondNumber;
+              answer = firstNumber / secondNumber;
               Console.WriteLine(name + " your answer is " + answer);
               break;
             }
@@ -92,7 +95,68 @@ namespace Calculator
           // Advance Calculator
           else if(which.ToLower() == "a")
           {
-
+            // Ask the user what operation, index or squareroot
+            Console.WriteLine(name + " type in 'i/s' to perform an (i)ndex or (s)quareroot operation");
+            operation = Console.ReadLine();
+            if(operation.ToLower() == "i")
+            {
+              //Prompt the user to type in their first number
+              Console.Write(name + ", can you please type your base number ");
+              //save it to this variable
+              firstEntry = Console.ReadLine();
+              // Prompt the user to type in their second number
+              Console.Write(name + ", can you please type your power number ");
+              //save it to this variable
+              secondEntry = Console.ReadLine();
+              // check if the user typed in an input which meets the requirements of the program, if not the program tells the user incorrect input was provided and restarts the program
+              try
+              {
+                // convert firstNumber and secondNumber into double
+                firstNumber = double.Parse(firstEntry);
+                secondNumber = double.Parse(secondEntry);
+              }
+              // if the console is unable to convert it into a double, incorrect input has been provided
+              catch(FormatException)
+              {
+                // Tell the user they have provided incorrect input, restart program
+                Console.WriteLine(name + ", please follow the instructions...");
+                Console.WriteLine("Restarting...");
+                continue;
+              }
+              answer = Math.Pow(firstNumber, secondNumber);
+              Console.WriteLine(name + " your answer is " + answer);
+              break;
+            }
+            else if(operation.ToLower() == "s")
+            {
+              //Prompt the user to type in their first number
+              Console.Write(name + ", can you please type the number you would like to squareroot ");
+              //save it to this variable
+              firstEntry = Console.ReadLine();
+              // Prompt the user to type in their second number
+              try
+              {
+                // convert firstNumber and secondNumber into double
+                firstNumber = double.Parse(firstEntry);
+              }
+              // if the console is unable to convert it into a double, incorrect input has been provided
+              catch(FormatException)
+              {
+                // Tell the user they have provided incorrect input, restart program
+                Console.WriteLine(name + ", please follow the instructions...");
+                Console.WriteLine("Restarting...");
+                continue;
+              }
+              answer = Math.Pow(firstNumber, 0.5);
+              Console.WriteLine(name + " your answer is " + answer);
+              break;
+            }
+            else
+            {
+              Console.WriteLine(name + ", please follow the instructions...");
+              Console.WriteLine("Restarting...");
+              continue;
+            }
           }
           else
           {
